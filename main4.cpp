81b7385438d73299b6d14b952aa0aaee7be1e302
nhir <origami.nhir@gmail.com>
2024-11-17 23:02:34 +0800

    main4.cpp,309行提交

--- main4.cpp
+++ main4.cpp
@@ -90,6 +90,7 @@ int main() {
     return 0;
 }
 */
+/*
 //P5734 【深基6.例6】文字处理软件
 #include <bits/stdc++.h>
 using namespace std;
@@ -112,7 +113,7 @@ int main() {
             case 2: {
                 int a,b;
                 cin >> a >> b;
-                input = input.substr(a,a+b);
+                input = input.substr(a,b);
                 cout << input << endl;
                 continue;
             }
@@ -137,3 +138,172 @@ int main() {
     }
     return 0;
 }
+*/
+/*
+//P1162 填涂颜色
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int arr[100][100];
+bool st[100][100];
+struct que {
+    int x;
+    int y;
+};
+void bfs(int a, int b) {
+    queue<struct que> q;
+    bool flag = true;
+    bool to2[100][100];
+    q.push({a, b});
+
+    while(!q.empty()) {
+        struct que p = q.front();
+        if (p.x == 1 || p.x == n ||  p.y == n || p.y == 1) {
+            flag = false;
+            // q.pop();
+            // break;
+        }
+        if(arr[p.x-1][p.y] == 0 && !st[p.x-1][p.y]) {
+            st[p.x-1][p.y] = true;
+            q.push({p.x-1, p.y});
+            to2[p.x-1][p.y] = true;
+        }
+        if(arr[p.x][p.y-1] == 0 && !st[p.x][p.y-1]) {
+            st[p.x][p.y-1] = true;
+            q.push({p.x, p.y-1});
+            to2[p.x][p.y-1] = true;
+        }
+        if(arr[p.x+1][p.y] == 0 && !st[p.x+1][p.y]) {
+            st[p.x+1][p.y] = true;
+            q.push({p.x+1, p.y});
+            to2[p.x+1][p.y] = true;
+        }
+        if(arr[p.x][p.y+1] == 0 && !st[p.x][p.y+1]) {
+            st[p.x][p.y+1] = true;
+            q.push({p.x, p.y+1});
+            to2[p.x][p.y+1] = true;
+        }
+        q.pop();
+    }
+    if(!flag) {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(to2[i][j])arr[i][j] = 2;
+                if(to2[i][j])st[i][j] = true;
+            }
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    // if(flag) {
+    //     for(int i = 1; i <= n; i++) {
+    //         for(int j = 1; j <= n; j++) {
+    //             if(to2[i][j])arr[i][j] = 2;
+    //             if(to2[i][j])st[i][j] = true;
+    //         }
+    //     }
+    // }
+    st[a][b] = true;
+}
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j =1 ; j <= n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    for (int i = 0; i <=n+1; i++) {
+        st[0][i] = true;
+        st[i][0] = true;
+        st[n+1][i] = true;
+        st[i][n+1] = true;
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (arr[i][j] == 0 && !st[i][j]) {
+                bfs(i,j);
+            }
+            //st[i][j] = true;
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+*/
+/*
+//P1162 填涂颜色
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int arr[100][100];
+struct que {
+    int x;
+    int y;
+};
+void bfs(int a, int b) {
+    queue<struct que> q;
+    bool to2[100][100] = {false};
+    q.push({a, b});
+    while(!q.empty()) {
+        struct que p = q.front();
+        if(p.x < 1 || p.y < 1 || p.x > n || p.y > n) {
+            q.pop();
+            continue;
+        }
+        if(arr[p.x-1][p.y] == 2 && !to2[p.x-1][p.y]) {
+            to2[p.x-1][p.y] = true;
+            q.push({p.x-1, p.y});
+        }
+        if(arr[p.x][p.y-1] == 2 && !to2[p.x][p.y-1]) {
+            to2[p.x][p.y-1] = true;
+            q.push({p.x, p.y-1});
+        }
+        if(arr[p.x+1][p.y] == 2 && !to2[p.x+1][p.y]) {
+            to2[p.x+1][p.y] = true;
+            q.push({p.x+1, p.y});
+        }
+        if(arr[p.x][p.y+1] == 2 && !to2[p.x][p.y+1]) {
+            to2[p.x][p.y+1] = true;
+            q.push({p.x, p.y+1});
+        }
+        q.pop();
+    }
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            if(to2[i][j])arr[i][j] = 0;
+        }
+    }
+}
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j =1 ; j <= n; j++) {
+            cin >> arr[i][j];
+            if (arr[i][j] == 0) arr[i][j] = 2;
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        if (arr[i][1] == 2) bfs(i, 1);
+        if (arr[i][n] == 2) bfs(i, n);
+        if (arr[1][i] == 2) bfs(1, i);
+        if (arr[n][i] == 2) bfs(n, i);
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+*/

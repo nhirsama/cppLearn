5ca43a3f68261fee55253d69c09a49a8273cda0c
nhir <origami.nhir@gmail.com>
2024-11-23 22:48:51 +0800

    更新main5.cpp至134行

--- Testlib.cpp
+++ Testlib.cpp
@@ -1,47 +1,29 @@
-//A - DIY
+//P4231 三步必杀
 #include <bits/stdc++.h>
 using namespace std;
-const int N=2e5+10;
-void Func() {
-    int v;
-    int arr2[N];
-    cin>>v;
-    for(int i =1 ; i <=v;i++) {
-        cin>>arr2[i];
-    }
-    sort(arr2+1,arr2+v+1);
-    int m = 0x7FFFFFFF;
-    int n = 0;
-    for(int i =1;i<=v;i++) {
-        if(arr2[i] == m) {
-            arr2[n++] = arr2[i];
-            m = 0x7FFFFFFF;
-            continue;
-        }
-        m = arr2[i];
-    }
-    if(n < 4) {
-        cout<<"NO\n";
-        return;
-    }
-    for(int i = 0 ; i < n/2;i++) {
-        for(int k = 0 ; k < 2;k++) {
-            int j = n-1-i+k;
-            cout<<"YES\n";
-            cout<< arr2[i] << ' '<< arr2[i+1] << ' '
-                << arr2[i] << ' ' << arr2[j] << ' '
-                << arr2[j-1] << ' ' << arr2[i+1] << ' '
-                << arr2[j-1] << ' ' << arr2[j] << '\n';
-            return;
-        }
-    }
-}
+int arr[10000005];
+int arr1[10000005];
 int main() {
-    ios_base::sync_with_stdio(0);
-    int n;
-    cin >> n;
-    for (int i =0 ; i < n ; i++) {
-        Func();
-    }
-    return 0;
+	int n,m;
+	scanf("%d %d",&n,&m);
+	while(m--) {
+		long long l,r,s,e;
+		scanf("%lld %lld %lld %lld",&l,&r,&s,&e);
+		int q = (e-s)/(r-l);
+		arr[l]+=s;
+		arr[r+1]-=e;
+		arr1[l+1]+=q;
+		arr1[r+1]-=q;
+	}
+	long long xorAns = 0,sum = 0,cF = 0,maxNum = 0;
+	for (int i = 1; i <= n; i++) {
+		cF += arr1[i];
+		sum += cF + arr[i];
+		xorAns ^= sum;
+		if (sum > maxNum) {
+			maxNum = sum;
+		}
+	}
+	printf("%lld %lld\n",xorAns,maxNum);
+	return 0;
 }
--- main5.cpp
+++ main5.cpp
@@ -1,4 +1,13 @@
 /*
+//模板
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+	return 0;
+}
+*/
+/*
 // CF1063B Labyrinth	//错误程序，改天重写一个
 #include <bits/stdc++.h>
 #define x first
@@ -55,6 +64,71 @@ int main(){
 		}
 	}
 	cout << ans;
+	return 0;
+}
+*/
+/*
+//P2367 语文成绩
+#include <bits/stdc++.h>
+using namespace std;
+int arr[5000010];
+int main() {
+	int n,p;
+	scanf("%d %d",&n,&p);
+	int a = 0;
+	for (int i = 1;i <= n;i++) {
+		scanf("%d",&arr[i]);
+		arr[i] -= a;
+		a += arr[i];
+	}
+	for (int i = 1; i <= p; i++) {
+		int x,y,z;
+		scanf("%d %d %d",&x,&y,&z);
+		arr[x] += z;
+		arr[y+1] -=z;
+	}
+	int ans = 0xFFFFFF,sum = 0;
+	for (int i = 1; i <= n; i++) {
+		sum += arr[i];
+		if (ans > sum)ans = sum;
+	}
+	cout << ans << endl;
+	return 0;
+}
+*/
+/*
+//P4231 三步必杀
+#include <bits/stdc++.h>
+using namespace std;
+int arr[10000005];
+int arr1[10000005];
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int n,m;
+	scanf("%d %d",&n,&m);
+	while(m--) {
+		long long l,r,s,e;
+		scanf("%lld %lld %lld %lld",&l,&r,&s,&e);
+		int q = (e-s)/(r-l);
+		arr[l]+=s;
+		arr[r+1]-=e;
+		arr1[l+1]+=q;
+		arr1[r+1]-=q;
+	}
+	long long xorAns = 0;
+	long long maxNum = arr[1];
+	long long sum = 0;
+	long long cF = 0;
+	for (int i = 1; i <= n; i++) {
+		cF += arr1[i];
+		sum += cF + arr[i];
+		xorAns ^= sum;
+		if (sum > maxNum) {
+			maxNum = sum;
+		}
+	}
+	printf("%lld %lld\n",xorAns,maxNum);
 	return 0;
 }
 */

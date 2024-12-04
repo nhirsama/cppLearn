7abc510439ae339f02b3b9c343bd80d600eeda44
Origami <origami.nhir@gmail.com>
2024-12-04 16:47:32 +0800

    新增LuoguOJ文件夹。

--- problem/LuoguOJ/P1955.cpp
+++ problem/LuoguOJ/P1955.cpp
@@ -1,12 +1,11 @@
-//模板
+//P1955 [NOI2015] 程序自动分析
 //记得把文件读写流和define注释掉
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    #define cin fin
-    #define cout fout
+#define cin fin
+#define cout fout
     ifstream fin("Testlib.in");
     ofstream fout("Code.out");
-
     return 0;
 }

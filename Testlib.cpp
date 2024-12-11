2763c661e97b0170f2463e5c22f64044ac5dceca
nhir <origami.nhir@gmail.com>
2024-12-11 15:12:53 +0800

    新增一道P3904，可以用于练习高精度

--- Testlib.cpp
+++ Testlib.cpp
@@ -1,18 +1,59 @@
-//#define LOCAL
-//模板
-//记得把文件重定向注释掉
-#include <bits/stdc++.h>
+//luoguP3904
+#include<iostream>
+#include<cmath>
 using namespace std;
-int main() {
-#ifdef LOCAL
-    freopen("Testlib.in", "r", stdin);
-    freopen("Code.out", "w", stdout);
-#endif
-    cout << (char)96<< (char)44 << (char)39;
-
-#ifdef LOCAL
-    fclose(stdin);
-    fclose(stdout);
-#endif
+const int maxn=55;
+int n,m;
+string s[maxn][maxn];
+string add(string a,string b)//高精度加法
+{
+    string ans;
+    int l1=a.size(),l2=b.size();
+    int lk=max(l1,l2);//选a和b里面长度最大的作为最终字符串长度
+    int u[maxn*10]={0},v[maxn*10]={0};
+    for(int i=0;i<l1;i++)u[l1-i-1]=a[i]-'0';
+    for(int i=0;i<l2;i++)v[l2-i-1]=b[i]-'0';//字符串转数组
+    for(int i=0;i<lk;i++)
+    {
+        u[i]+=v[i];
+        u[i+1]+=u[i]/10;
+        u[i]%=10;
+    }//模拟加法运算
+    if(u[lk])lk++;//当出现进位时，长度++
+    for(int i=lk-1;i>=0;i--)ans+=u[i]+'0';//重新转成字符串
+    return ans;
+}
+string mul(string a,int b)//高精a*单精b（大部分与加法相同，故不做注释）
+{
+    string ans;
+    int l=a.size();
+    int u[maxn*10]={0};
+    for(int i=l-1;i>=0;i--)u[l-i-1]=a[i]-'0';
+    int w=0;
+    for(int i=0;i<l;i++)
+    {
+        u[i]=u[i]*b+w;
+        w=u[i]/10;
+        u[i]%=10;
+    }
+    while(w)u[l++]=w%10,w/=10;//注意这里与加法不同，是while循环而不是if特判，想一想为什么？
+    for(int i=l-1;i>=0;i--)ans+=u[i]+'0';
+    return ans;
+}
+int main()
+{
+    cin>>n>>m;
+    for(int i=1;i<=n;i++)s[i][1]="1";//初始化
+    for(int i=1;i<=n;i++)
+    {
+        s[i][i]="1";
+        for(int j=2;j<=m;j++)
+        {
+            s[i][j]=add(s[i-1][j-1],mul(s[i-1][j],j));//递推方程
+        }
+    }
+    if(n<m)cout<<"0"<<endl;//注意特判
+    else cout<<s[n][m]<<endl;
     return 0;
 }
+//6220194750
--- problem/LuoguOJ/P3904.cpp
+++ problem/LuoguOJ/P3904.cpp
@@ -89,3 +89,69 @@ int main() {
     return 0;
 }
 */
+#include <bits/stdc++.h>
+using namespace std;
+vector <char> add(vector <char> aa,vector <char> bb){
+    if(aa.size()<bb.size())return add(bb,aa);
+    for(int i=  0;i<aa.size();i++){
+        if(i<bb.size())aa[i] += bb[i];
+        if(aa[i]>=10){
+            if (i == aa.size()-1)aa.push_back(1);
+            else aa[i+1]++;
+            aa[i] %=10;
+        }
+    }
+    return aa;
+}
+vector <char> mul(vector <char> aa,int b){
+    vector <char> bb;
+    while (b>0) {
+        bb.push_back(b%10);
+        b/=10;
+    }
+    vector<char> c(aa.size()+bb.size()+1,0);
+    for(int i = 0;i<aa.size();i++){
+        for(int j = 0;j<bb.size();j++){
+            c[i+j] += aa[i]*bb[j];
+            c[i+j+1] += c[i+j]/10;
+            c[i+j] %= 10;
+        }
+    }
+    while(c.size()>1 && c.back() == 0)c.pop_back();
+    return c;
+}
+int main() {
+#ifdef LOCAL
+    freopen("Testlib.in", "r", stdin);
+    freopen("Code.out", "w", stdout);
+#endif
+    vector<char> dp[55][55];
+    int n,m;
+    cin>>n>>m;
+    if(n<m) {
+        cout << 0 << endl;
+        return 0;
+    }
+    if(n==m) {
+        cout<<1<<endl;
+        return 0;
+    }
+    for (int i = 1; i <= n; i++) {
+        dp[i][1]={1};
+    }
+    for (int i = 1; i <= n; i++) {
+        dp[i][i]={1};
+        for (int j = 2; j <= m; j++) {
+            dp[i][j] = add(dp[i-1][j-1], mul(dp[i-1][j],j));
+        }
+    }
+    for (int i = dp[n][m].size()-1; i >= 0; i--) {
+        cout << (int)dp[n][m][i];
+    }
+    cout << endl;
+#ifdef LOCAL
+    fclose(stdin);
+    fclose(stdout);
+#endif
+    return 0;
+}

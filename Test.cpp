ef4b3388409a359e02b99523eb783008b91b1f29
nhir <origami.nhir@gmail.com>
2024-12-24 22:01:55 +0800

    更新了一下对拍程序

--- Test.cpp
+++ Test.cpp
@@ -1,10 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
+    clock_t beginTime;
+    clock_t endTime;
+    beginTime = clock();
     string sout = {""}; //放入程序输出文件路径
-    if (!sout.size()) {
-        cout << "请输入与Code.out文件对拍的路径:"<<endl;
-        cin>>sout;
+    if (sout.empty()) {
+        ifstream ifile;
+        ifile.open("Testlib.out");
+        string line;
+        ifile >> line;
+        //可以在Testlib.out中输入正确答案的文件相对路径，或者输入正确答案。
+        if (line.empty()) {
+            cout << "请输入与Code.out文件对拍的路径:"<<endl;
+            cin>>sout;
+        }
+        else {
+            if (line.substr(0,13) == "inAndoutFile/") sout = line;
+            else sout = "Testlib.out";
+        }
+        ifile.close();
     }
     ifstream Ffout,Tfout;
     Tfout.open(sout, ios::in);
@@ -12,17 +27,24 @@ int main() {
     string s;
     int t = 1;
     bool flag = true;
-    while(Tfout>>s){
+    while(Tfout>>s) {
         string s1;
-        Ffout >> s1;
+        if (!(Ffout >> s1)) {
+            cout << "程序输出过短，再检查一遍吧" <<endl;
+            flag = false;
+            break;
+        };
         if(s1 != s) {
             cout<<"在"<<t<<"行与正确答案不符"<<endl;
             flag = false;
         }
         t++;
     }
-    if (flag) cout<<"宝宝好棒，程序输出正确"<<endl;
+    if (Ffout>>s) cout<<"输出过长，再重新检测一下吧"<<endl;
+    else if (flag) cout<<"宝宝好棒，程序输出正确"<<endl;
     Tfout.close();
     Ffout.close();
+    endTime = clock();
+    cout <<"本程序运行时间为"<< static_cast<double>(endTime - beginTime) << "毫秒" << endl;
     return 0;
 }

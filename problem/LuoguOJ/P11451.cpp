1528e50cc71affa5b52efdea2d0a95115e6ef468
nhir <origami.nhir@gmail.com>
2025-02-18 00:17:00 +0800

    新增114514

--- problem/LuoguOJ/P11451.cpp
+++ problem/LuoguOJ/P11451.cpp
@@ -1,58 +1,48 @@
 //P11451 [USACO24DEC] It's Mooin' Time B
 #include <bits/stdc++.h>
 using namespace std;
-char trie[27][27][27];
-int main(){
+
+int main() {
     string s;
-    int n,k;
-    cin>>n>>k;
-    cin>>s;
-    set<string >ans;
-    for(int i = 0;i<n-2;i++){
-        trie[s[i]-'a'][s[i+1]-'a'][s[i+2]-'a']++;
-    }
-    for(int i = 0;i<26;i++){
-        for(int j = 0;j<26;j++){
-            if(trie[i][j][j]>=k && i!= j){
-                string aaa;
-                aaa.push_back('a'+i);
-                aaa.push_back('a'+j);
-                aaa.push_back('a'+j);
-                ans.insert(aaa);
-            }
-            for(int m = 0;m<26;m++) {
-                if(trie[i][j][m] == k-1){
-                    for(int l = 0;l<26;l++){
-                        if(trie[l][j][m]>=1 && l !=i && j == m){
-                            string aaa;
-                            aaa.push_back('a'+i);
-                            aaa.push_back('a'+j);
-                            aaa.push_back('a'+j);
-                            ans.insert(aaa);
-                        }
-                        if(trie[i][l][m]>=1 && l !=i && l == m){
-                            string aaa;
-                            aaa.push_back('a'+i);
-                            aaa.push_back('a'+j);
-                            aaa.push_back('a'+j);
-                            ans.insert(aaa);
-                        }
-                        if(trie[i][j][l]>=1 && l !=i && l == j){
-                            string aaa;
-                            aaa.push_back('a'+i);
-                            aaa.push_back('a'+j);
-                            aaa.push_back('a'+j);
-                            ans.insert(aaa);
-                        }
-                    }
+    int n, k;
+    cin >> n >> k;
+    cin >> s;
+    set<string> ans;
+    for (int i = 'a'; i <= 'z'; i++) {
+        for (int j = 'a'; j <= 'z'; j++) {
+            if (i == j)continue;
+            int cnt = 0;
+            vector<bool> st(n, false);
+            for (int m = 0; m < n - 2; m++) {
+                if ((s[m] == i && s[m + 1] == j && s[m + 2] == j)) {
+                    cnt++;
+                    st[m] = true;
+                    st[m + 1] = true;
+                    st[m + 2] = true;
+                    m += 2;
                 }
+            }
+            for (int m = 0; m < n - 2; m++) {
+                if (st[m] || st[m + 1] || st[m + 2]) continue;
+                if ((s[m] == i && s[m + 1] == j) ||
+                    (s[m] == i && s[m + 2] == j) ||
+                    (s[m + 1] == j && s[m + 2] == j)) {
+                    cnt++;
+                    break;
+                }
+            }
+            if (cnt >= k) {
+                string t;
+                t.push_back(i);
+                t.push_back(j);
+                t.push_back(j);
+                ans.insert(t);
             }
         }
     }
-    //sort(ans.begin(),ans.end());
-    cout<<ans.size()<<endl;
-    for(auto i:ans){
-        cout<<i<<'\n';
+    cout << ans.size() << endl;
+    for (auto i: ans) {
+        cout << i << '\n';
     }
     return 0;
 }

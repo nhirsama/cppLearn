736df68c94cb4b2472b48d728b8b8c89b77d8183
nhir <origami.nhir@gmail.com>
2025-02-27 13:30:50 +0800

    upd:glo27

--- problem/CodeForcesOJ/glo27/B.cpp
+++ problem/CodeForcesOJ/glo27/B.cpp
@@ -9,14 +9,25 @@ typedef long long int ll;
 #define space << ' ' <<
 typedef pair<int, int> pii;
 
-void Solve(){
-
+void Solve() {
+    int n;
+    cin >> n;
+    if (n == 1 || n == 3)cout << -1 << endl;
+    else if (n == 2) cout << 66 << endl;
+    else if (n & 1) {
+        for (int i = 1; i <= n - 5; i++) cout << 3;
+        cout << 36366 << endl;
+    } else {
+        for (int i = 1; i <= n - 4; i++) cout << 3;
+        cout << 3366 << endl;
+    }
 }
+
 int main() {
-    #ifdef LOCAL
+#ifdef LOCAL
     freopen("Testlib.in", "r", stdin);
     //freopen("Code.out", "w", stdout);
-    #endif
+#endif
     int T;
     cin >> T;
     while (T--) {
--- problem/CodeForcesOJ/glo27/C.cpp
+++ problem/CodeForcesOJ/glo27/C.cpp
@@ -6,20 +6,26 @@ typedef long long int ll;
 #define x first
 #define y second
 #define endl '\n'
-#define space << ' ' <<
+#define space << ' '
 typedef pair<int, int> pii;
+void Solve(){
+    int n;
+    cin>>n;
+    if(n&1){
+        cout<<n<<endl;
 
-void Solve() {
+    }
+    else {
+        int num = (1<<((int)log2(n)+1))-1;
+        cout<<num<<endl;
 
+    }
 }
-
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);cout.tie(nullptr);
-#ifdef LOCAL
+    #ifdef LOCAL
     freopen("Testlib.in", "r", stdin);
     //freopen("Code.out", "w", stdout);
-#endif
+    #endif
     int T;
     cin >> T;
     while (T--) {

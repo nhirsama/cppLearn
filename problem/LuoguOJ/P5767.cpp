7dbe9ca7994273245dd4688c150ae7c341707efd
Origami <nhirsama@outlook.com>
2025-03-07 14:13:28 +0800

    upd:一道图论

--- problem/LuoguOJ/P5767.cpp
+++ problem/LuoguOJ/P5767.cpp
@@ -8,25 +8,57 @@ typedef long long int ll;
 #define endl '\n'
 #define space << ' '
 typedef pair<int, int> pii;
+vector<int> dist(N+1,0x3f3f3f3f),st(N+1);
 int main() {
     if (getenv("ONLINE_JUDGE") == nullptr) {
         freopen("Testlib.in", "r", stdin);
         freopen("Code.out", "w", stdout);
     }
-    int m,n,be,en;
-    cin>>m>>n>>be>>en;
-    vector g(n+1,vector<pii>());
-    for(int i = 1;i<=n;i++){
+    int m,n;
+    cin>>m>>n;
+    vector g(n+1,vector<int>());
+    for(int i = 0;i<=n;i++){
         string s;
         getline(cin,s);
         istringstream cinl(s);
-        int u,v;
-        cinl>>u;
+        int v;
+        vector<int> r;
         while(cinl>>v){
-            g[u].push_back({v,0});
-            u = v;
+            r.push_back(v);
         }
-
+        for(int j=0;j<r.size();j++){
+            for(int k=j+1;k<r.size();k++){
+                g[r[j]].push_back(r[k]);
+            }
+        }
+    }
+    auto spfa =  [&](){
+        st[1] = 1;
+        dist[1] = 0;
+        queue<int> q;
+        q.push(1);
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            st[u] = 0;
+            for(auto v : g[u]){
+                if(dist[v] > dist[u]+1){
+                    dist[v] = dist[u]+1;
+                    if(!st[v]){
+                        st[v] = 1;
+                        q.push(v);
+                    }
+                }
+            }
+        }
+    };
+    spfa();
+    int ans = dist[n];
+    if(ans > 0x3f3f3f3f/2) {
+        cout<<"NO"<<endl;
+    }
+    else{
+        cout<<ans-1<<endl;
     }
     return 0;
 }

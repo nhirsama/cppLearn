4786ad07a7ac8869ed7f86a72e8ac0a0ec14cbed
nhir <origami.nhir@gmail.com>
2025-03-09 21:54:28 +0800

    upd:3.5小时的史

--- problem/LuoguOJ/P10312.cpp
+++ problem/LuoguOJ/P10312.cpp
@@ -1,4 +1,4 @@
-//模板
+//P10312 [SHUPC 2024] 栅栏密码
 #include <bits/stdc++.h>
 using namespace std;
 constexpr int N = 1e5 + 10;
@@ -15,6 +15,20 @@ int main() {
     }
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
+    int h;
+    cin>>h;
+    string s;
+    cin>>s;
+    vector<vector<char> > vv(h+1);
+    int cnt = 1;
+    for(char c:s){
+        vv[cnt].push_back(c);
+        if(cnt<h) cnt++;
+        else cnt = 1;
+    }
+    for(int i  =1;i<=h;i++){
+      for(char c:vv[i])
+        cout<<c;
+    }
     return 0;
 }
--- src/Testlib.cpp
+++ src/Testlib.cpp
@@ -1,59 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-const int N = 1005;
-const int mod = 1e9 + 7;
-ll arr[N][N], base[N][N];
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int n, m, q;
-    cin >> n >> m >> q;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            cin >> base[i][j];
+void Func(){
+    string s;
+    int m,k;
+    cin>>s>>m;
+    int le = s.size()/m;
+    int yv = s.size()%m;
+    k = 1;
+    while(k--){
+        string temp;
+        for(int i =0;i<le;i++){
+            for(int j = 0;j<m;j++){
+                if(j<yv){
+                    if(j*(le+1)+i >= s.size())continue;
+                    temp.push_back(s[j*(le+1)+i]);
+                }
+                else{
+                    if(j*le+i+yv >= s.size())continue;
+                    temp.push_back(s[j*le+i+yv]);
+                }
+            }
         }
-    }
-    memset(arr, 0, sizeof(arr));
-    while (q--) {
-        int x1, y1, x2, y2, c;
-        cin >> x1 >> y1 >> x2 >> y2 >> c;
-        arr[x1][y1] += c;
-        arr[x1][y2 + 1] -= c;
-        arr[x2 + 1][y1] -= c;
-        arr[x2 + 1][y2 + 1] += c;
-    }
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            if (j > 1) arr[i][j] += arr[i][j - 1];
+        for(int i =1;i<=yv;i++){
+            temp.push_back(s[le*i]);
         }
+        s = temp;
     }
-    for (int j = 1; j <= m; j++) {
-        for (int i = 1; i <= n; i++) {
-            if (i > 1) arr[i][j] += arr[i - 1][j];
-        }
-    }
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            arr[i][j] = (base[i][j] + arr[i][j]) % mod;
-            arr[i][j] = arr[i][j] * arr[i][j] % mod;
-        }
-    }
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= m; j++) {
-            arr[i][j] = (arr[i][j] + arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1]) % mod;
-            arr[i][j] = (arr[i][j] + mod) % mod;
-        }
-    }
-    int Q;
-    cin >> Q;
-    while (Q--) {
-        int x1, y1, x2, y2;
-        cin >> x1 >> y1 >> x2 >> y2;
-        ll ans = (arr[x2][y2] - arr[x1 - 1][y2] - arr[x2][y1 - 1] + arr[x1 - 1][y1 - 1]) % mod;
-        ans = (ans + mod) % mod;
-        cout << ans << '\n';
+    cout<<s<<endl;
+}
+int main(){
+    int n = 1;
+    while(n--){
+        Func();
     }
     return 0;
 }

9d7b9fefc0433537fd0278130a8d3b2fbe6a8725
Origami <nhirsama@outlook.com>
2025-04-09 00:45:03 +0800

    upd:新增一场比赛

--- problem/CodeForcesOJ/1016_div3/F.cpp
+++ problem/CodeForcesOJ/1016_div3/F.cpp
@@ -5,7 +5,7 @@ using i64 = long long int;
 using i32 = int;
 typedef std::pair<i32, i32> pii;
 constexpr int N = 1e5 + 10;
-constexpr int mod = 1e9+7;
+constexpr int mod = 1e9 + 7;
 #define IOS std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
 #define x first
 #define y second
@@ -13,7 +13,47 @@ constexpr int mod = 1e9+7;
 #define all(x) (x).begin(), (x).end()
 
 void nhir() {
-
+    i32 n, m;
+    std::cin >> n >> m;
+    std::vector<std::string> a(n);
+    std::vector b(m, std::vector<std::string>(n));
+    for (auto &i: a) {
+        std::cin >> i;
+    }
+    for (auto &i: b) {
+        for (auto &j: i) {
+            std::cin >> j;
+        }
+    }
+    for (i32 j = 0; j < n; j++) {
+        bool flag = false;
+        for (i32 i = 0; i < m; i++) {
+            if (b[i][j] == a[j]) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) {
+            std::cout << -1 << endl;
+            return;
+        }
+    }
+    i32 aa = 0;
+    for (i32 i = 0; i < m; i++) {
+        i32 cnt = 0;
+        for (i32 j = 0; j < n; j++) {
+            if (b[i][j] == a[j])
+                cnt++;
+        }
+        aa = std::max(aa, cnt);
+    }
+    i32 ans = 0;
+    if (n <= aa) {
+        ans = n;
+    } else {
+        ans = 3 * n - 2 * aa;
+    }
+    std::cout << ans << endl;
 }
 
 signed main() {
@@ -23,7 +63,7 @@ signed main() {
         freopen("Testlib.in", "r", stdin);
         // freopen("Code.out", "w", stdout);
     }
-    std::cin>>T;
+    std::cin >> T;
     while (T--) nhir();
     return 0;
 }

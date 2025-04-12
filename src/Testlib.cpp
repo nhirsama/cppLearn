33c189e0edeea4fad896eda52a4e1d669270b0ef
Origami <nhirsama@outlook.com>
2025-04-12 10:46:39 +0800

    upd:init from to Origami.

--- src/Testlib.cpp
+++ src/Testlib.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-  std::cout<<1510553%1000007<<std::endl;
-  return 0;
+#define ll long long
+
+const int MAX_N = 200000;
+const int MAX_K = 1;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    for (int i = 0;i<=100;i++){
+        
+    }
+
+    return 0;
 }

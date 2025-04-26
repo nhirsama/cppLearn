7cd3bb9dd5cea471f10fc1d583c6d78676212938
nhir <origami.nhir@gmail.com>
2025-04-26 14:47:32 +0800

    upd: from to nhir

--- problem/LanQiao/20250426/baoli.cpp
+++ problem/LanQiao/20250426/baoli.cpp
@@ -1,52 +1,58 @@
-#include<bits/stdc++.h>
-using i64 = long long int ;
-int dp[2000][2]; //
-int main() {
-	int n,m;
-	std::cin>>n>>m;
+#include <bits/stdc++.h>
 
-	std::vector<std::string> g(n);
-	for(int i = 0; i<n; i++) {
-		std::cin>>g[i];
-	}
-	i64 ans[4][2]{};
-	for(int i = 0; i<n; i++) {
-		int sum = 0;
-		for(int j = 0; j<m; j++) {
-			int cnt000 = 0,cnt010 = 0,cnt110 = 0,cnt100 = 0;
-			if(i>0) if(g[i-1][j] == g[i][j]) cnt000++;
-			if(i<n-1) if(g[i+1][j] == g[i][j]) cnt000++;
-			if(j>0) if(g[i][j-1] == g[i][j]) cnt000++;
-			if(j<m-1) if(g[i][j+1] == g[i][j]) cnt000++;
-			
-			if(i>0) if(g[i-1][j] != g[i][j]) cnt100++;
-			if(i<n-1) if(g[i+1][j] == g[i][j]) cnt100++;
-			if(j>0) if(g[i][j-1] == g[i][j]) cnt100++;
-			if(j<m-1) if(g[i][j+1] == g[i][j]) cnt100++;
-			
-			if(i>0) if(g[i-1][j] == g[i][j]) cnt110++;
-			if(i<n-1) if(g[i+1][j] != g[i][j]) cnt110++;
-			if(j>0) if(g[i][j-1] == g[i][j]) cnt110++;
-			if(j<m-1) if(g[i][j+1] == g[i][j]) cnt110++;
-			
-			if(i>0) if(g[i-1][j] != g[i][j]) cnt010++;
-			if(i<n-1) if(g[i+1][j] != g[i][j]) cnt010++;
-			if(j>0) if(g[i][j-1] == g[i][j]) cnt010++;
-			if(j<m-1) if(g[i][j+1] == g[i][j]) cnt010++;
-			ans[0][1] = ans[0][0]+cnt000*cnt000;
-			ans[2][1] = ans[3][0]+cnt010*cnt010;
-			ans[1][1] = 
-			
-			ans+=std::max(std::max(cnt000*cnt000,cnt100*cnt100),std::max(cnt010*cnt010,cnt110*cnt110));
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	vector<vector<int>> grid(n, vector<int>(m));
+	for(int i = 0; i < n; i++) {
+		string str;
+		cin >> str;
+		for(int j = 0; j < m; j++) {
+			grid[i][j] = str[j] - '0';
 		}
 	}
-	std::cout<<ans;
+	// Calculate row_contrib
+	vector<int> row_contrib(n, 0);
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < m - 1; j++) {
+			if(grid[i][j] == grid[i][j + 1]) row_contrib[i] += 2;
+		}
+	}
+	if(n == 1) {
+		cout << row_contrib[0] << endl;
+		return 0;
+	}
+	// DP
+	long long prev0 = row_contrib[0];
+	long long prev1 = row_contrib[0];
+	for(int i = 1; i < n; i++) {
+		// Calculate col_contrib
+		long long col00 = 0; // t=0, s=0
+		for(int j = 0; j < m; j++) {
+			if(grid[i - 1][j] == grid[i][j]) col00 += 2;
+		}
+		long long col01 = 0; // t=0, s=1
+		for(int j = 0; j < m; j++) {
+			if(grid[i - 1][j] == (1 - grid[i][j])) col01 += 2;
+		}
+		long long col10 = 0; // t=1, s=0
+		for(int j = 0; j < m; j++) {
+			if((1 - grid[i - 1][j]) == grid[i][j]) col10 += 2;
+		}
+		long long col11 = 0; // t=1, s=1
+		for(int j = 0; j < m; j++) {
+			if((1 - grid[i - 1][j]) == (1 - grid[i][j])) col11 += 2;
+		}
+		// Calculate dp[i][0]
+		long long dp_i_0 = max(prev0 + col00, prev1 + col10) + row_contrib[i];
+		// Calculate dp[i][1]
+		long long dp_i_1 = max(prev0 + col01, prev1 + col11) + row_contrib[i];
+		// Update prev
+		prev0 = dp_i_0;
+		prev1 = dp_i_1;
+	}
+	cout << max(prev0, prev1) << endl;
 	return 0;
 }
-/*
-4 4
-1010
-1111
-1011
-1100
- */
--- problem/LanQiao/20250426/test.cpp
+++ problem/LanQiao/20250426/test.cpp
@@ -2,11 +2,11 @@
 using i64 = long long int ;
 int main(){
 	for(int i = 1;i<1000;i++){
-		std::cout<<"juding on test "<<i<<std::endl;
+		std::cout<<"judging on test "<<i<<std::endl;
 		std::system("testlib.exe > data.in");
-		std::system("E.exe < data.in > E.out");
-		std::system("E.exe < data.in > baoli.out");
-		if(std::system("fc E.out baoli.out")){
+		std::system("G2.exe < data.in > G.out");
+		std::system("baoli.exe < data.in > baoli.out");
+		if(std::system("fc G.out baoli.out")){
 			std::cout<<"wrong answer on test" << i<<std::endl;
 			return 0;
 		}

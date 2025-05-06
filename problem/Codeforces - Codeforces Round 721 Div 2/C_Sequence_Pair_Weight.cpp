0aa10171e2b420fc4b8df4b11cd9ea84949e74dd
nhir <origami.nhir@gmail.com>
2025-05-06 22:47:27 +0800

    upd:QWQ from to nhir

---
+++ problem/Codeforces - Codeforces Round 721 Div 2/C_Sequence_Pair_Weight.cpp
@@ -0,0 +1,35 @@
+//  C. Sequence Pair Weight
+#include <bits/stdc++.h>
+
+using i64 = long long int;
+using i32 = long long int;
+using __i_know_i_need_int = int;
+constexpr __i_know_i_need_int mod = 1e9 + 7;
+#define int __i_cannot_use_int
+#define IOS std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
+#define endl '\n'
+
+
+void nhir() {
+    i32 n;
+    std::cin >> n;
+    std::map<i32, i32> m;
+    std::vector<i32> dp(n + 1);
+    i32 ans = 0;
+    for (i32 i = 1; i <= n; i++) {
+        i32 x;
+        std::cin >> x;
+        dp[i] = dp[i - 1] + m[x];
+        m[x] += i;
+        ans += dp[i];
+    }
+    std::cout << ans << endl;
+}
+
+signed main() {
+    IOS;
+    __i_know_i_need_int T = 1;
+    std::cin >> T;
+    while (T--) nhir();
+    return 0;
+}

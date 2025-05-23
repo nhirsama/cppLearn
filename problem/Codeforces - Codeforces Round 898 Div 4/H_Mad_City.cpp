1c339a1e09ecc2e439c18e5889ddaaf732797482
nhir <origami.nhir@gmail.com>
2025-05-23 20:24:24 +0800

    upd:QWQ from to nhir

--- problem/Codeforces - Codeforces Round 898 Div 4/H_Mad_City.cpp
+++ problem/Codeforces - Codeforces Round 898 Div 4/H_Mad_City.cpp
@@ -11,7 +11,64 @@ constexpr __i_know_i_need_int mod = 1e9 + 7;
 
 
 void nhir() {
-    
+    i32 n, a, b;
+    std::cin >> n >> a >> b;
+    a--, b--;
+    std::vector g(n, std::vector<i32>());
+    for (i32 i = 0; i < n; i++) {
+        i32 u, v;
+        std::cin >> u >> v;
+        u--, v--;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    if (a == b) {
+        std::cout << "NO\n";
+        return;
+    }
+    std::vector<i32> vis(n, -1);
+    i32 sta = -1;
+    vis[b] = 0;
+    auto dfs1 = [&](auto &&self, i32 u, i32 f) -> bool {
+        for (auto v: g[u]) {
+            if (v == f) continue;
+            if (vis[v] != -1) {
+                sta = v;
+                return true;
+            }
+            vis[v] = vis[u] + 1;
+            if (self(self, v, u)) return true;
+        }
+        return false;
+    };
+    dfs1(dfs1, b, b);
+    std::vector<i32> dis(n, 1E18);
+    auto dijkstra = [&](i32 s) -> void {
+        using PII = std::pair<i32, i32>;
+        std::priority_queue<PII, std::vector<PII>, std::greater<PII>> q;
+        q.emplace(0, s);
+        dis[s] = 0;
+        std::vector<i32> viss(n + 1);
+        while (!q.empty()) {
+            i32 x = q.top().second;
+            q.pop();
+            if (viss[x]) continue;
+            viss[x] = 1;
+            for (auto y: g[x]) {
+                if (dis[y] > dis[x] + 1) {
+                    dis[y] = dis[x] + 1;
+                    q.emplace(dis[y], y);
+                }
+            }
+        }
+    };
+    dijkstra(a);
+//    for (auto i: vis) {
+//        std::cout << i << ' ';
+//    }
+//    std::cout << '\n';
+//    std::cout << sta << ' ' << vis[sta] << ' ' << dis[sta] << '\n';
+    std::cout << (dis[sta] <= vis[sta] ? "NO\n" : "YES\n");
 }
 
 signed main() {

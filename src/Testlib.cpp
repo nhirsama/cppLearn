430148cd854982364780865f7a427e06e6ff51b4
nhir <origami.nhir@gmail.com>
2025-06-09 13:57:02 +0800

    upd:呜呜呜 from to nhir

--- src/Testlib.cpp
+++ src/Testlib.cpp
@@ -1,14 +1,51 @@
+// code by Elaina
 #include <bits/stdc++.h>
-
-int main() {
-    std::random_device rd;
-    std::mt19937 mt(rd());
-    int cnta = 0, cntb = 0;
-    while (std::abs(cnta - cntb) <= 5) {
-        auto ran = mt();
-        if (ran & 1) cnta++;
-        else cntb++;
+#define cno cout << "NO\n"
+#define cyes cout << "YES\n"
+#define endl '\n'
+#define N 200010
+using namespace std;
+using ll = long long;
+ll t, n, ans;
+ll a[N], b[N],p1[N],p2[N];
+void solve()
+{
+    ans = 0;
+    cin >> n;
+    memset(p1, -1, sizeof(p1));
+    memset(p2, -1, sizeof(p2));
+    for (int i = 1; i <= n; ++i)
+        cin >> a[i];
+    for (int i = 1; i <= n; ++i)
+        cin >> b[i];
+    for (int i = n; i >= 1; --i)
+    {
+        if(a[i]==b[i])
+        {
+            ans=i;
+            break;
+        }
+        if(p1[a[i]]!=-1||p2[b[i]]!=-1){
+            ans = i;
+            break;
+        }
+        else if(p1[b[i]]>i+1||p2[a[i]]>i+1){
+            ans = i;
+            break;
+        }
+        p1[a[i]] = i, p2[b[i]] = i;
     }
-    std::cout << std::format("{}", (cntb > cnta ? "去二餐" : "去南门")) << '\n';
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    freopen("Code.out", "r", stdin);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> t;
+    while (t--)
+        solve();
     return 0;
 }
--- src/hackCode.cpp
+++ src/hackCode.cpp
@@ -1,55 +1,15 @@
-//模板
 #include <bits/stdc++.h>
-using i128 = __int128;
-using i64 = long long int;
-using LL = long long int;
-using i32 = int;
-typedef std::pair<i32, i32> pii;
-constexpr int N = 1e5 + 10;
-using namespace std;
-constexpr int mod = 1e9+7;
-#define IOS std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr);
-#define x first
-#define y second
-#define endl '\n'
-#define all(x) (x).begin(), (x).end()
-
-void nhir() {
-    int n;
-    cin >> n;
-    vector<LL>arr(n + 1);
-    map<LL , LL>mp;
-    for(int i = 1 ; i <= n ; i ++){
-        cin >> arr[i];
-    }
-
-    LL res = 0;
-
-    for(int i = 1 ; i <= n ; i ++){
-        LL x = 0;
-        cin >> x;
-        while(!mp[x]){
-            mp[x] = 1;
-            res ++;
-            x = arr[x];
-        }
-
-        cout<<res<<" ";
-    }
-    cout<<"\n";
-}
 
 signed main() {
-    IOS;
-    const clock_t beginTime = clock();
-    i32 T = 1;
-    if (getenv("LOCAL") != nullptr) {
-        freopen("Testlib.in", "r", stdin);
-        freopen("Code.out", "w", stdout);
+    std::cout << 10000 << std::endl;
+    for (int i = 0; i < 1e4; i++) {
+        std::cout << 20 << std::endl;
+        for (int j = 0; j < 20; j++) {
+            std::cout << 20 << " \n"[j == 19];
+        }
+        for (int j = 0; j < 20; j++) {
+            std::cout << 19 << " \n"[j == 19];
+        }
     }
-    std::cin>>T;
-    while (T--) nhir();
-    const clock_t endTime = clock();
-    std::cout << "本程序运行时间为" << static_cast<double>(endTime - beginTime) << "毫秒" << endl;
     return 0;
 }

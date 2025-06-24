e906510af302dbf54e64172fb72f023aac67f7d6
nhir <origami.nhir@gmail.com>
2025-06-24 23:02:23 +0800

    upd:呜呜呜 from to nhir

--- problem/Codeforces - Codeforces Round 1024 Div 1/A_Mex_in_the_Grid.cpp
+++ problem/Codeforces - Codeforces Round 1024 Div 1/A_Mex_in_the_Grid.cpp
@@ -6,13 +6,40 @@ using i32 = long long int;
 constexpr int mod = 1e9 + 7;
 constexpr char endl = '\n';
 constexpr i64 inf_i64 = 0x3f3f3f3f3f3f3f3f;
+i32 dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
+i32 dy[] = {1, 1, 0, -1, -1, -1, 0, 1};
 
 void nhir() {
-    
+    i32 n;
+    std::cin >> n;
+    std::vector g(n, std::vector<i32>(n, -1));
+    i32 tot = 0;
+    std::deque<std::array<i32, 2>> q;
+    q.push_front({n / 2, n / 2});
+    g[n / 2][n / 2] = tot++;
+    while (!q.empty()) {
+        auto [x, y] = q.front();
+        q.pop_front();
+        for (i32 i = 0; i < 8; i++) {
+            i32 ix = x + dx[i];
+            i32 iy = y + dy[i];
+            if (ix >= n || ix < 0 || iy >= n || iy < 0) continue;
+            if (g[ix][iy] != -1)continue;
+            g[ix][iy] = tot++;
+            q.push_front({ix, iy});
+        }
+    }
+    for (i32 i = 0; i < n; i++) {
+        for (i32 j = 0; j < n; j++) {
+            std::cout << g[i][j] << ' ';
+        }
+        std::cout << endl;
+    }
 }
 
 signed main() {
-    std::ios::sync_with_stdio(false); std::cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
     int T = 1;
     std::cin >> T;
     while (T--) nhir();

94059a557b1e5e065a463ee4f5e3d84b9d4c23c2
nhir <origami.nhir@gmail.com>
2025-06-26 23:00:10 +0800

    upd:呜呜呜 from to nhir

--- problem/Codeforces - Codeforces Round 1033 Div 2 and CodeNite 2025/C_Divine_Tree.cpp
+++ problem/Codeforces - Codeforces Round 1033 Div 2 and CodeNite 2025/C_Divine_Tree.cpp
@@ -8,6 +8,39 @@ constexpr char endl = '\n';
 constexpr i64 inf_i64 = 0x3f3f3f3f3f3f3f3f;
 
 void nhir() {
+    i32 n, m;
+    std::cin >> n >> m;
+    if (m < n || m > (n * (1 + n) / 2)) {
+        std::cout << -1 << endl;
+        return;
+    }
+    i32 t = n;
+    if (m <= 2 * n - 1) {
+        std::cout << m - (n - 1) << endl;
+        t = m - (n - 1);
+        for (i32 i = 1; i <= n; i++) {
+            if (i == t) continue;
+            if (i == 1) {
+                std::cout << t << ' ' << i << endl;
+                continue;
+            }
+            std::cout << 1 << ' ' << i << endl;
+        }
+        return;
+    }
+    std::cout << n << endl;
+    m -= n;
+    while (--n) {
+        if (n == 1) {
+            std::cout << 1 << ' ' << t << endl;
+        } else if (m >= n + n - 1) {
+            std::cout << t << ' ' << n << endl;
+            m -= n;
+        } else {
+            std::cout << 1 << ' ' << n << endl;
+            m--;
+        }
+    }
 }
 
 signed main() {

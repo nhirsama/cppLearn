fc10099bf2f28d87b4997bd4d86175c6a57094db
nhir <origami.nhir@gmail.com>
2025-07-11 01:21:41 +0800

    upd:呜呜呜 from to nhir

--- problem/Codeforces - Codeforces Round 1024 Div 1/A_Mex_in_the_Grid.cpp
+++ problem/Codeforces - Codeforces Round 1024 Div 1/A_Mex_in_the_Grid.cpp
@@ -8,8 +8,12 @@ constexpr char endl = '\n';
 constexpr i64 inf_i64 = 0x3f3f3f3f3f3f3f3f;
 i32 dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
 i32 dy[] = {1, 1, 0, -1, -1, -1, 0, 1};
+std::array<i32, 2> tl[] = {{0,  1},
+                           {1,  0},
+                           {0,  -1},
+                           {-1, 0}};
 
-void nhir() {
+void nhir1() {
     i32 n;
     std::cin >> n;
     std::vector g(n, std::vector<i32>(n, -1));
@@ -32,6 +36,35 @@ void nhir() {
     for (i32 i = 0; i < n; i++) {
         for (i32 j = 0; j < n; j++) {
             std::cout << g[i][j] << ' ';
+        }
+        std::cout << endl;
+    }
+}
+
+void nhir() {
+    i32 n;
+    std::cin >> n;
+    std::vector g(n, std::vector<i32>(n, -1));
+    i32 x = (n - 1) / 2, y = (n - 1) / 2;
+    i32 it = 0, t = 0;
+    for (; it < n * n; it++) {
+        g[x][y] = it;
+        i32 ix = x + tl[(t) % 4][0], iy = y + tl[(t) % 4][1];
+        if (ix < n && ix >= 0 && iy < n && iy >= 0 && g[ix][iy] == -1) {
+            x = ix, y = iy;
+            t++;
+        } else {
+            ix = x + tl[(t + 3) % 4][0], iy = y + tl[(t + 3) % 4][1];
+            x = ix, y = iy;
+        }
+//        if (x + tl[(t + 3) % 4][0] >= n || x + tl[(t + 3) % 4][0] < 0 || y + tl[(t + 3) % 4][1] >= n ||
+//            y + tl[(t + 3) % 4][0] < 1) {
+//
+//        }
+    }
+    for (auto &i: g) {
+        for (auto j: i) {
+            std::cout << j << ' ';
         }
         std::cout << endl;
     }

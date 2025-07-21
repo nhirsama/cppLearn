7bbc0abf552c314e900fcb5d2a70864398d33a25
nhir <origami.nhir@gmail.com>
2025-07-21 20:24:11 +0800

    upd:QWQ from to nhir

--- problem/Nowcoder/2025_Nowcoder_Multi_University_Training_Contest_2/Bitwise_Perfect.cpp
+++ problem/Nowcoder/2025_Nowcoder_Multi_University_Training_Contest_2/Bitwise_Perfect.cpp
@@ -9,7 +9,26 @@ constexpr char enld = '\n';
 constexpr i64 inf_i64 = 0x3f3f3f3f3f3f3f3f;
 
 void nhir() {
-
+    i32 n;
+    std::cin >> n;
+    std::vector<i32> v(n);
+    for (i32 &i: v)std::cin >> i;
+    if (n > 60) {
+        std::cout << "NO\n";
+        return;
+    } else {
+        for (i32 i = 0; i < n; i++) {
+            for (i32 j = i + 1; j < n; j++) {
+                if (i == j) continue;
+                if (std::max(v[i], v[j]) >= (v[i] ^ v[j])) {
+                    std::cout << "NO\n";
+                    return;
+                }
+            }
+        }
+    }
+    std::cout << "YES\n";
+    return;
 }
 
 signed main() {

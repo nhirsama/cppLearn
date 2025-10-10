4970ec31f46f2016bf285eb3fa0e767aa6d5329a
nhirsama <nhirsama@outlook.com>
2025-10-10 21:37:29 +0800

    呜呜呜

---
+++ problem/Codeforces - Educational Codeforces Round 183 Rated for Div 2/A_Candies_for_Nephews.cpp
@@ -0,0 +1,23 @@
+//  A. Candies for Nephews
+//  20:45
+#include <bits/stdc++.h>
+
+using int64 = int64_t;
+using i32 = int64_t;
+constexpr int mod = 1e9 + 7;
+constexpr char endl = '\n';
+constexpr char enld = '\n';
+constexpr int64 inf_i64 = 0x3f3f3f3f3f3f3f3f;
+constexpr std::string OJ_NAME{"Codeforces"};
+void nhir() {
+    
+}
+
+signed main() {
+    std::ios::sync_with_stdio(false); std::cin.tie(nullptr);
+    int T = 1;
+    if (OJ_NAME != "AtCoder")
+    std::cin >> T;
+    while (T--) nhir();
+    return 0;
+}

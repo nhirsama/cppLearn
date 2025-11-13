ed7d6c76b18620c238898c27a666546c9dc1c331
nhirsama <nhirsama@outlook.com>
2025-11-13 22:02:20 +0800

    QWQ

---
+++ problem/Luogu/P10416__2023__A_XYZ.cpp
@@ -0,0 +1,31 @@
+//  P10416 [蓝桥杯 2023 国 A] XYZ
+//  20:26
+#include <bits/stdc++.h>
+
+using int64 = int64_t;
+using i32 = int64_t;
+constexpr int mod = 1e9 + 7;
+constexpr char endl = '\n';
+constexpr char enld = '\n';
+constexpr int64 inf_i64 = 0x3f3f3f3f3f3f3f3f;
+constexpr std::string OJ_NAME{"Luogu"};
+
+void nhir() {
+    int64 l, r;
+    std::cin >> l >> r;
+    if (r - 2 * l < 0) {
+        std::cout << 0 << enld;
+        return;
+    }
+    std::cout << (r - 2 * l + 1) * (r - 2 * l + 2) / 2 << endl;
+}
+
+signed main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int T = 1;
+    if (OJ_NAME != "AtCoder")
+        std::cin >> T;
+    while (T--) nhir();
+    return 0;
+}

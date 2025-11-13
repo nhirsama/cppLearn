856da935e7a99d84cc1e15b725aa035fc23aacda
ling <nhirsama@outlook.com>
2025-11-13 17:47:25 +0800

    add code

---
+++ problem/Codeforces - Codeforces Round 1063 Div 2/A_Souvlaki_VS_Kalamaki.cpp
@@ -0,0 +1,38 @@
+//  A. Souvlaki VS. Kalamaki
+//  16:57
+#include <bits/stdc++.h>
+
+using int64 = int64_t;
+using i32 = int64_t;
+constexpr int mod = 1e9 + 7;
+constexpr char endl = '\n';
+constexpr char enld = '\n';
+constexpr int64 inf_i64 = 0x3f3f3f3f3f3f3f3f;
+constexpr std::string OJ_NAME{"Codeforces"};
+
+void nhir() {
+    int n;
+    std::cin >> n;
+    std::vector<int> v(n);
+    for (auto &i: v) std::cin >> i;
+    std::ranges::sort(v);
+    for (int i = 0; i < n - 1; i++) {
+        if (i & 1) {
+            if (v[i] != v[i + 1]) {
+                std::cout << "NO\n";
+                return;
+            }
+        }
+    }
+    std::cout << "YES\n";
+}
+
+signed main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int T = 1;
+    if (OJ_NAME != "AtCoder")
+        std::cin >> T;
+    while (T--) nhir();
+    return 0;
+}

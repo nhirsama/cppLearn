9ccf2e8ca72be85be3f83f1a984e469ebc79baf1
ling <nhirsama@outlook.com>
2025-12-10 21:18:01 +0800

    .

---
+++ problem/Codeforces - Codeforces Round 1069 Div 2/A_Little_Fairys_Painting.cpp
@@ -0,0 +1,36 @@
+//  A. Little Fairy's Painting
+//  17:59
+
+#include <bits/stdc++.h>
+
+using int64 = int64_t;
+using i32 = int64_t;
+constexpr int mod = 1e9 + 7;
+constexpr char endl = '\n';
+constexpr char enld = '\n';
+constexpr int64 inf_i64 = 0x3f3f3f3f3f3f3f3f;
+constexpr std::string OJ_NAME{"Codeforces"};
+
+void nhir() {
+    int n;
+    std::cin >> n;
+    std::map<int, int> m;
+    std::vector<int> v(n);
+    for (auto &i: v) std::cin >> i;
+    for (auto &i: v) {
+        m[i]++;
+    }
+    std::ranges::sort(v);
+    auto it = std::ranges::lower_bound(v, m.size());
+    std::cout << *it << endl;
+}
+
+signed main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int T = 1;
+    if (OJ_NAME != "AtCoder")
+        std::cin >> T;
+    while (T--) nhir();
+    return 0;
+}

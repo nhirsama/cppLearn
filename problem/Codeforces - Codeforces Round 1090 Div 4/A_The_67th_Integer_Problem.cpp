afe9c82d5c3a9830b3bc38a6aff6a2f8a7c6d45c
ling <rin@nhirs.com>
2026-04-05 17:07:00 +0800

    add

---
+++ problem/Codeforces - Codeforces Round 1090 Div 4/A_The_67th_Integer_Problem.cpp
@@ -0,0 +1,27 @@
+//  A. The 67th Integer Problem
+//  14:10
+#include <bits/stdc++.h>
+
+using int64 = int64_t;
+using i32 = int64_t;
+constexpr int mod = 1e9 + 7;
+constexpr char endl = '\n';
+constexpr char enld = '\n';
+constexpr int64 inf_i64 = 0x3f3f3f3f3f3f3f3f;
+constexpr std::string OJ_NAME{"Codeforces"};
+
+void nhir() {
+    int n;
+    std::cin >> n;
+    std::cout << 67 << enld;
+}
+
+signed main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    int T = 1;
+    if (OJ_NAME != "AtCoder")
+        std::cin >> T;
+    while (T--) nhir();
+    return 0;
+}
